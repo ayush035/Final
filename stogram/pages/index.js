f01486c93cd5196abfad1a5a193f34d829a8a3d2
Ayush Gupta <ayushguptaknp2080@gmail.com>
2024-04-21 04:09:47 +0530

    frontemd2

--- stogram/pages/index.js
+++ stogram/pages/index.js
@@ -4,6 +4,7 @@ import Navbar from '@/components/Navbar'
 // import Upload from '@/components/Upload'
 // const inter = Inter({ subsets: ['latin'] })
 import d from '../public/d.jpg'
+import e from '../public/e.jpeg'
 import shield from '../public/shield.png'
 import Footer from '@/components/Footer'
 
@@ -12,7 +13,15 @@ export default function Home() {
   return (
    <>
 <Navbar/>
-<div className='grid grid-cols-2'>
+<div className ='flex justify-center items-center my-20 mx-20'>
+<Image 
+src={e}
+alt=''
+width="1100"
+height="400"/>
+
+</div>
+{/* <div className='grid grid-cols-2'>
         <div className=' flex flex-col=-2'>
       <div className ='my-20 mx-20 transition ease-in-out delay-50 hover:-translate-y-1 hover:scale-105'>
 <Image 
@@ -31,16 +40,7 @@ width="350"
 height="400"/>
 </div>
 </div> 
-</div>
-{/* <div className ='my-20 mx-20'>
-<Image 
-src={e}
-alt=''
-width="1100"
-height="400"/>
-
-</div>  */}
-{/* <Footer/> */}
+</div> */}
    </>
   )
 }
