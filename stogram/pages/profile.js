dfc8e06b401b75e58de0ca87b1d4b70a7835e1aa
Ayush Gupta <ayushguptaknp2080@gmail.com>
2024-04-23 19:07:41 +0530

    front end

--- stogram/pages/profile.js
+++ stogram/pages/profile.js
@@ -2,7 +2,7 @@ import React, { useState, useReducer } from 'react'
 import Image from 'next/image'
 import Navbar from '../components/Navbar'
 import { Web3Storage } from 'web3.storage'
-import Logo from '../public/Logo.png'
+import stogram from '../public/stogram.jpg'
 import pic from '../public/pic.jpg'
 import Footer from '../components/Footer'
 
@@ -33,7 +33,7 @@ export default function profile()
   <div className=' bg-white shadow-2xl text-white mx-20 my-2 rounded-xl outline outline-offset-2 outline-pink-300 outline-width:4px '>
                 <div className="mx-1 my-1 cursor-pointer ">
                     <a href='https://bafybeietu4bacniu3vfiparmvxqmd4oclzdiieqva2gaz2exyif46ne4tu.ipfs.dweb.link/Logo.png'>
-                    <Image src={Logo} 
+                    <Image src={stogram} 
                     alt='' height="260px" width='400px'>
                     </Image></a>
                     </div>
@@ -46,7 +46,7 @@ export default function profile()
                     </div>
                     </div>
 
-                    <div className=' bg-white shadow-2xl text-white mx-20 my-2 rounded-xl outline outline-offset-2 outline-pink-300 outline-width:4px '>
+                    {/* <div className=' bg-white shadow-2xl text-white mx-20 my-2 rounded-xl outline outline-offset-2 outline-pink-300 outline-width:4px '>
                 <div className="mx-1 my-1 cursor-pointer">
                 <a href='https://bafybeiab2w2lb6dugorkaxoi3fsl7666bh2m2erxpk7ahvgi5voxvxcvtu.ipfs.dweb.link/photo_2023-04-28_20-29-12.jpg'>
                     <Image src={pic} 
@@ -60,7 +60,7 @@ export default function profile()
                         </a>
                     </div>
                     </div>
-                    </div>
+                    </div> */}
 
 
     </div>
