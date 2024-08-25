e544a108a40fe3ad45c04fe41da7d78874c6c5a3
Ayush Gupta <ayushguptaknp2080@gmail.com>
2024-08-26 04:02:49 +0530

    Stake feature front end update
    front end update

--- stogram/pages/donate1.js
+++ stogram/pages/donate1.js
@@ -30,7 +30,15 @@ Donate
 <button className ="mx-10 font-mono text-white text-xl font-bold bg-pink-400 rounded-lg my-2 px-2 py-1">
  Donate 
    </button>
- 
+   <div className= "mx-10 font-mono text-black text-xl font-bold">
+Stake on NFT to earn rewards
+</div>
+<div>
+<input className='mx-10 font-mono pb-2 text-black text-xl bg-gray-200 rounded-lg my-4' placeholder='  $USDC ' type='number' />
+</div>
+<button className ="mx-10 font-mono text-white text-xl font-bold bg-pink-400 rounded-lg my-2 px-2 py-1">
+ Stake
+   </button>
 
 </div>
 
