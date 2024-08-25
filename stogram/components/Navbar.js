199e148cd8192c42dfaa768c39a9b80a6995c333
Ayush Gupta <ayushguptaknp2080@gmail.com>
2024-08-25 18:27:02 +0530

    Update Navbar.js

--- stogram/components/Navbar.js
+++ stogram/components/Navbar.js
@@ -7,11 +7,11 @@ export default function Navbar() {
     return (
         
             <>
-                        <div className=" h-6 my-4 px-8  text-black mx-16 rounded-lg font-mono font-semibold text-center" >
+                        {/* <div className=" h-6 my-4 px-8  text-black mx-16 rounded-lg font-mono font-semibold text-center" >
             We are live on Gitcoin Grants Round 21 ! Donate <Link href="https://shorturl.at/Q3P6O" legacyBehavior><a className="text-pink-500">here ↗️</a></Link>
 
             </div>
-            <hr  />
+            <hr  /> */}
 
         
             <nav className=' flex justify-between h-12 text-pink-500 bg-white   font-bold' >
