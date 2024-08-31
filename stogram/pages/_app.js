ddfee2c339b02ecf32063eceffa1c40cb4af9d20
Ayush Gupta <ayushguptaknp2080@gmail.com>
2024-09-01 03:38:20 +0530

    Update _app.js

--- stogram/pages/_app.js
+++ stogram/pages/_app.js
@@ -12,14 +12,33 @@ import {
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { WagmiProvider, http } from 'wagmi'
 import { mainnet, zetachain, zksync, arbitrum, zetachainAthensTestnet } from 'wagmi/chains'
-import { getDefaultConfig } from '@rainbow-me/rainbowkit'
+import { getDefaultConfig, } from '@rainbow-me/rainbowkit'
+
+const avalanche = {
+  id: 5555,
+  name: 'CVC Kura',
+
+  iconBackground: '#fff',
+  nativeCurrency: { name: 'XCR', symbol: 'XCR', decimals: 18 },
+  rpcUrls: {
+    default: { http: ['https://rpc-kura.cross.technology'] },
+  },
+  blockExplorers: {
+    default: { name: 'Crossvaluescan', url: 'https://testnet.crossvaluescan.com' },
+  },
+  // contracts: {
+  //   multicall3: {
+  //     address: '0xca11bde05977b3631167028862be2a173976ca11',
+  //     blockCreated: 11907934,
+  //   },
+  }
 
 
 
 const config = getDefaultConfig({
   appName: 'RainbowKit demo',
   projectId: '9c17dc69becbe137fe50e55e31598852',
-  chains: [zetachain, zksync, arbitrum, zetachainAthensTestnet],
+  chains: [zetachain, zksync, arbitrum, zetachainAthensTestnet,avalanche],
   transports: {
     [arbitrum.id]: http(),
     [zetachain.id]: http(),
@@ -36,8 +55,8 @@ const queryClient = new QueryClient()
 function MyApp({ Component, pageProps }) {
   return (
 <WagmiProvider config={config}>
-<QueryClientProvider client={queryClient}>
-        <RainbowKitProvider
+<QueryClientProvider  client={queryClient}>
+        <RainbowKitProvider 
         initialChain={4}
         theme={lightTheme({
           accentColor: '#FF69B4',
