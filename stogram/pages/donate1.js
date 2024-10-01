9bcfb1d5181e6d231eb0da139f87f215adba6f4c
Ayush Gupta <ayushguptaknp2080@gmail.com>
2024-10-01 19:15:19 +0530

    Update donate1.js

--- stogram/pages/donate1.js
+++ stogram/pages/donate1.js
@@ -35,7 +35,7 @@ Donate
 Stake on NFT to earn rewards
 </div>
 <div>
-<input className='mx-10 font-mono pb-2 text-black text-xl bg-gray-200 rounded-lg my-4' placeholder='  $USDC ' type='number' />
+<input className='mx-10 font-mono pb-2 text-black text-xl bg-gray-200 rounded-lg my-4' placeholder=' Coming soon!'  />
 </div>
 <button className ="mx-10 font-mono text-white text-xl font-bold bg-pink-400 rounded-lg my-2 px-2 py-1">
  Stake
